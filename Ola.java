d336a7d3819e5c04a5f45fcb8415f933d2ba07a5
Matheus Souza Komo <matheus.sbs03@gmail.com>
2025-04-10 16:33:24 -0300

    Add UI

--- Ola.java
+++ Ola.java
@@ -1,5 +1,7 @@
+import javax.swing.JOptionPane;
 public class Ola{
     public static void main (String[] args){
-        System.out.println("Boa tarde à todos!");
+       //System.out.println("Boa tarde à todos!");
+       JOptionPane.showMessageDialog(null, "Boa tarde à todos!");
     }
 }
